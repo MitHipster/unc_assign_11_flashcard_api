56570a9b7edbefc31b5c62969dd9743974759072
Tim Acker <tjacker31@gmail.com>
2017-08-02 13:44:10 -0400

    Cloze Testing
    Added testing examples for cloze constructor and refactored cloze js to remove a parameter and fix a function call.

--- cloze.js
+++ cloze.js
@@ -1,25 +1,25 @@
 /*jslint esversion: 6, browser: true*/
 const fs = require('fs');
 
-let Cloze = function(fullText, cloze, choice) {
-  this.partialText(fullText, cloze, choice);
+let Cloze = function(fullText, cloze, test) {
+  this.partialText = this.partial(fullText, cloze, test);
   this.fullText = fullText;
   this.cloze = cloze;
 };
 
-Cloze.prototype.partialText = function (fullText, cloze, choice) {
+Cloze.prototype.partial = function (fullText, cloze, test) {
   if (fullText.toLowerCase().includes(cloze.toLowerCase())) {
-    let partial = fullText.replace(cloze, '...');
-    this.log(partial, cloze);
-    return partial;
+    let text = fullText.replace(cloze, '...');
+    if(!test) this.log(text, cloze);
+    return text;
   } else {
     console.error('Cloze not found in full text. Please re-enter.\n');
   }
 };
 
-Cloze.prototype.log = function(partialText, cloze) {
+Cloze.prototype.log = function(text, cloze) {
   let card =
-    'Full statement: ' + partialText + '\n' +
+    'Full statement: ' + text + '\n' +
     'Cloze deletion: ' + cloze + '\n\n';
   fs.appendFile('cloze.txt', card, function (error) {
     if (error) throw error;
--- test.js
+++ test.js
@@ -5,7 +5,18 @@ const Cloze = require('./cloze.js');
 let firstPresident = new Basic('Who was the first president of the United States?', 'George Washington', true);
 
 // Who was the first president of the United States?
-console.log('\n' + firstPresident.front);
+console.log('\nBasic: \n' + firstPresident.front);
 
 // George Washington
 console.log(firstPresident.back + '\n');
+
+let firstPresidentCloze = new Cloze('George Washington was the first president of the United States.', 'George Washington', true);
+
+// George Washington
+console.log('Cloze: \n' + firstPresidentCloze.cloze);
+
+// ... was the first president of the United States.
+console.log(firstPresidentCloze.partialText);
+
+// George Washington was the first president of the United States.
+console.log(firstPresidentCloze.fullText + '\n');
